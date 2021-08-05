a414b91fad17761a57b1b8049ff76eb3644929cc
davidw <61147505+davidwingqvist@users.noreply.github.com>
2021-08-06 01:45:09 +0200

    Add files via upload

---
+++ np_assignment2/clientmain.cpp
@@ -0,0 +1,266 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* You will to add includes here */
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+
+// Included to get the support library
+#include <calcLib.h>
+
+#include "protocol.h"
+int terminate = 0; // How many times we will redo loop until someone responds
+int loopCount = 0; // Counts in seconds
+
+// Does calculation on integers and returns an integer
+int integer_calculate(int first, int second, int calcType)
+{
+  int result;
+  if(calcType == 1) // add
+  {
+    result = (int)first + (int)second;
+  }
+  else if(calcType == 2) // sub
+  {
+    result = (int)first - (int)second;
+  }
+  else if(calcType == 3) // mul
+  {
+    result = (int)(first * second);
+  }
+  else if(calcType == 4) // div
+  {
+    result = (int)(first / second);
+  }
+    // Do calculations then return it as result
+  printf("(client)integer result: %d\n", result); // TO BE REMOVED
+  return result;
+}
+
+// Does calculation on double and returns a double
+double float_calculate(double first, double second, int calcType)
+{
+  double result;
+    if(calcType == 5) // fadd
+    {
+      result = first + second;
+    }
+    else if(calcType == 6) // fsub
+    {
+      result = first - second;
+    }
+    else if(calcType == 7) // fmul
+    {
+      result = first * second;
+    }
+    else if(calcType == 8) // fdiv
+    {
+      result = first / second;
+    }
+
+    printf("(client)float result: %lf\n", result); // TO BE REMOVED
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2)
+  {
+    printf("No more/less than 1 argument needs to be present. Retry.\n");
+    exit(0);
+  }
+  
+  calcMessage cliBuf;
+  calcProtocol cliResp; // Client Response
+
+  calcProtocol servBuf;
+
+  // Translated package details
+  int type = 0;
+  int minor_version = 0;
+  int major_version = 0;
+  int id = 0;
+  int arith = 0;
+  int inVal1 = 0;
+  int inVal2 = 0;
+  int inRes = 0;
+  double val1 = 0, val2 = 0, fresult = 0;
+  int result = 0;
+
+  socklen_t servLen = 0;
+  bool packRecv = false; // Confirmation that package recieved
+  bool packSent = false;
+
+  // Set up our package to be sen
+  cliBuf.type = htons(22); // client to server - 22
+  cliBuf.message = htonl(0);
+  cliBuf.protocol = htons(17); // UDP - 17
+  cliBuf.major_version = htons(1);
+  cliBuf.minor_version = htons(0);
+
+ struct addrinfo hints, *servinfo, *p;
+
+  // Divide string into two parts 
+  char* adress = strtok(argv[1], ":");
+  char* port = strtok(NULL, "");
+  printf("Host %s ", adress);
+  printf("and port %s\n", port);
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_CANONNAME;
+  
+  int rv;
+  if((rv = getaddrinfo(adress, port, &hints, &servinfo)) != 0)
+  {
+      perror("Adress info");
+      exit(0);
+  }
+
+  int sockfd;
+  for(p = servinfo; p != NULL; p = p->ai_next)
+  {
+      if((sockfd = socket(p->ai_family, p->ai_socktype,
+                        p->ai_protocol)) == -1)
+      {
+        perror("Listener : Socket");
+        continue;
+      }
+/*
+      if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+      {
+        close(sockfd);
+        perror("Listener : Connect");
+        continue;
+      }
+*/
+      break;
+  }
+
+  freeaddrinfo(servinfo);
+
+  // Send over package to server
+  int len = sendto(sockfd, (const void*)&cliBuf, sizeof(cliBuf),
+            0, p->ai_addr, p->ai_addrlen);
+
+  if(len == -1)
+  {
+    perror("Package failed");
+  }
+
+  while(terminate <= 5)
+  {
+    sleep(1);
+    loopCount++;
+
+    // Wait cycle resets after certain amount of time
+    if(loopCount >= 2)
+    {
+      int n = recvfrom(sockfd, &servBuf, sizeof(servBuf), MSG_DONTWAIT, p->ai_addr, &p->ai_addrlen);
+      if(n >= (int)sizeof(calcProtocol))
+      {
+        type = ntohs(servBuf.type);
+        minor_version = ntohs(servBuf.minor_version);
+        major_version = ntohs(servBuf.major_version);
+        id = ntohl(servBuf.id);
+        arith = ntohl(servBuf.arith);
+        inVal1 = ntohl(servBuf.inValue1);
+        inVal2 = ntohl(servBuf.inValue2);
+        val1 = servBuf.flValue1;
+        val2 = servBuf.flValue2;
+        packRecv = true; // confirmation package was recieved
+
+        // Load in the float values
+        if(arith >= 5)
+        {
+          printf("(Server)First: %lf, Second: %lf\n", val1, val2);
+          fresult = float_calculate(val1, val2, arith); // Calculate the result of arith sent by server
+        }
+        else // else load in the integer values
+        {
+          printf("(Server)First: %d, Second: %d\n", inVal1, inVal2);
+          result = integer_calculate(inVal1, inVal2, arith);
+        }
+
+        // Set up calcProtocol to resend result
+        cliResp.type = htons(22);
+        cliResp.minor_version = htons(0);
+        cliResp.major_version = htons(1);
+        cliResp.id = htonl(id);
+        cliResp.arith = htonl(arith);
+        cliResp.inValue1 = htonl(inVal1);
+        cliResp.inValue2 = htonl(inVal2);
+        cliResp.inResult = htonl(result);
+        cliResp.flValue1 = val1;
+        cliResp.flValue2 = val2;
+        cliResp.flResult = fresult;
+
+        len = sendto(sockfd, (const void*)&cliResp, sizeof(cliResp),
+            0, p->ai_addr, p->ai_addrlen);
+        packSent = true;
+      }
+      else
+      {
+        if(!packRecv)
+        {
+          // Displays error message to user.
+          calcMessage* error = (calcMessage*)&servBuf;
+          int error_type = ntohs(error->type);
+          int error_message = ntohl(error->message);
+          int error_major = ntohs(error->major_version);
+          int error_minor = ntohs(error->minor_version);
+
+          if(error_type == 2 && error_message == 2 && error_major == 1 &&
+          error_minor == 0)
+          {
+            printf("(Server)NOT OK!\n");
+            close(sockfd);
+            exit(0);
+          }
+        }
+        if(packSent)
+        {
+          calcMessage* response = (calcMessage*)&servBuf;
+          int resp_message = ntohl(response->message);
+          // Correct answer
+          if(resp_message == 1)
+          {
+            printf("(Server)OK\n");
+            close(sockfd);
+            exit(0);
+          }
+          else if(resp_message == 2) // Incorrect answer
+          {
+            printf("(Server)NOT OK\n");
+            close(sockfd);
+            exit(0);
+          }
+        }
+      }
+      loopCount = 0;
+      terminate++;
+      
+      // Make sure we send resend it 2 times which adds up to three with initial transmission.
+      if(!packRecv && terminate < 3)
+      {
+        printf("Resending package...\n");
+        // Resend package
+        len = sendto(sockfd, (const void*)&cliBuf, sizeof(cliBuf),
+              0, p->ai_addr, p->ai_addrlen);
+      }
+    }
+  }
+  if(!packRecv || !packSent)
+  {
+    printf("Server timed out.\n");
+  }
+  else
+    printf("End of session.\n");
+  close(sockfd);
+  exit(0);
+}
---
+++ np_assignment2/servermain.cpp
@@ -0,0 +1,471 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+/* You will to add includes here */
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+// Included to get the support library
+#include <calcLib.h>
+
+#include "protocol.h"
+
+using namespace std;
+/* Needs to be global, to be rechable by callback and main */
+int loopCount=0;
+int terminate_loop=0;
+
+// Store information
+// Public for the purpose of ease of use with certain functions
+vector<calcProtocol> storage;
+vector<sockaddr> addresses;
+vector<int> timers;
+vector<timeval> tim;
+int current_pos = 0;
+
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if(sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+bool is_in_system(int ID, int &d)
+{
+  for(int i = 0; i < (int)storage.size(); i++)
+  {
+    if((int)htonl(storage[i].id) == ID)
+    {
+      d = i;
+      return true;
+    }
+  }
+
+  return false; // Didn't find ID
+}
+
+void decrement_timers()
+{
+  for(int i = 0; i < (int)timers.size(); i++)
+  {
+    if(timers[i] > 0)
+    {
+      timers[i] -= 1;
+    }
+  }
+}
+
+void check_timers()
+{
+  for(int i = 0; i < (int)timers.size(); i++)
+  {
+    if(timers[i] == 0)
+    {
+      current_pos--;
+      timers[i] = -1;
+    }
+  }
+}
+
+void empty_storage()
+{
+    storage.clear();
+    timers.clear();
+    addresses.clear();
+    tim.clear();
+  printf("Storage has been ransacked.\n");
+}
+
+void create_package(calcProtocol &calc)
+{
+  calc.type = htons(1);
+  calc.major_version = htons(1);
+  calc.minor_version = htons(0);
+  calc.id = htonl(randomInt());
+  calc.flValue1 = 0.0;
+  calc.flValue2 = 0.0;
+  calc.inValue1 = htonl(0);
+  calc.inValue2 = htonl(0);
+
+  char* type_arith = randomType();
+  if(strcmp("fsub", type_arith) == 0)
+  {
+    calc.arith = 6;
+    calc.flValue1 = randomFloat();
+    calc.flValue2 = randomFloat();
+    calc.flResult = calc.flValue1 - calc.flValue2;
+  }
+  else if(strcmp("fadd", type_arith) == 0)
+  {
+    calc.arith = 5;
+    calc.flValue1 = randomFloat();
+    calc.flValue2 = randomFloat();
+    calc.flResult = calc.flValue1 + calc.flValue2;
+  }
+  else if(strcmp("fmul", type_arith) == 0)
+  {
+    calc.arith = 7;
+    calc.flValue1 = randomFloat();
+    calc.flValue2 = randomFloat();
+    calc.flResult = calc.flValue1 * calc.flValue2;
+  }
+  else if(strcmp("fdiv", type_arith) == 0)
+  {
+    calc.arith = 8;
+    calc.flValue1 = randomFloat();
+    calc.flValue2 = randomFloat();
+    calc.flResult = calc.flValue1 / calc.flValue2;
+  }
+  else if(strcmp("add", type_arith) == 0)
+  {
+    calc.arith = 1;
+    calc.inValue1 = randomInt();
+    calc.inValue2 = randomInt();
+    calc.inResult = htonl((calc.inValue1 + calc.inValue2));
+
+    calc.inValue1 = htonl(calc.inValue1);
+    calc.inValue2 = htonl(calc.inValue2);
+  }
+  else if(strcmp("sub", type_arith) == 0)
+  {
+    calc.arith = 2;
+    calc.inValue1 = randomInt();
+    calc.inValue2 = randomInt();
+    calc.inResult = htonl((calc.inValue1 - calc.inValue2));
+
+    calc.inValue1 = htonl(calc.inValue1);
+    calc.inValue2 = htonl(calc.inValue2);
+  }
+  else if(strcmp("mul", type_arith) == 0)
+  {
+    calc.arith = 3;
+    calc.inValue1 = randomInt();
+    calc.inValue2 = randomInt();
+    calc.inResult = htonl((calc.inValue1 * calc.inValue2));
+
+    calc.inValue1 = htonl(calc.inValue1);
+    calc.inValue2 = htonl(calc.inValue2);
+  }
+  else if(strcmp("div", type_arith) == 0)
+  {
+    calc.arith = 4;
+    calc.inValue1 = randomInt();
+    calc.inValue2 = randomInt();
+    calc.inResult = htonl((calc.inValue1 / calc.inValue2));
+
+    calc.inValue1 = htonl(calc.inValue1);
+    calc.inValue2 = htonl(calc.inValue2);
+  }
+
+  calc.arith = htonl(calc.arith);
+}
+
+// Check if an address is already inside the database
+bool is_in_system(sockaddr check)
+{
+  char compare[20] = {0};
+  inet_ntop(check.sa_family, &check, (char*)compare, INET6_ADDRSTRLEN);
+  for(int i = 0; i < (int)addresses.size(); i++)
+  {
+    char in_store[20] = {1}; 
+    inet_ntop(addresses[i].sa_family, &addresses[i], (char*)in_store, INET_ADDRSTRLEN);
+    if(strstr(compare, in_store) != NULL)
+    {
+      return true;
+    }
+  }
+  // Add address to storage
+  addresses.push_back(check);
+  return false; // Address was not in system
+}
+
+
+  /* Call back function, will be called when the SIGALRM is raised when the timer expires. */
+  void checkJobbList(int signum)
+  {
+  // As anybody can call the handler, its good coding to check the signal number that called it.
+  if(signum == SIGALRM)
+  {
+    struct timeval comp;
+    gettimeofday(&comp, NULL);
+    int first = comp.tv_sec;
+    for(int i = 0; i < (int)timers.size(); i++)
+    {
+      int second = timers[i];
+      int res = first - second;
+      if(first - second >= 10)
+      {
+        if(timers[i] != -1)
+          current_pos--;
+          
+        timers[i] = -1;
+      }
+    }
+  }
+    return;
+  }
+
+
+int main(int argc, char *argv[]){
+  
+  if(argc != 2)
+  {
+    printf("No more/less than 1 argument needs to be present. Retry.\n");
+    exit(0);
+  }
+  /* Do more magic */
+  calcProtocol servBuf;
+  sockaddr servaddr = {0};
+  struct addrinfo hints, *servinfo, *p;
+
+  // Divide string into two parts 
+  char* adress = strtok(argv[1], ":");
+  char* port = strtok(NULL, "");
+  printf("Host %s ", adress);
+  printf("and port %s\n", port);
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_CANONNAME;
+  
+  int rv;
+  if((rv = getaddrinfo(adress, port, &hints, &servinfo)) != 0)
+  {
+      perror("Adress info");
+      exit(0);
+  }
+
+  int sockfd;
+  for(p = servinfo; p != NULL; p = p->ai_next)
+  {
+      if((sockfd = socket(p->ai_family, p->ai_socktype,
+                        p->ai_protocol)) == -1)
+      {
+        perror("Listener : Socket");
+        continue;
+      }
+
+      if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+      {
+        close(sockfd);
+        perror("Listener : Bind");
+        continue;
+      }
+
+      break;
+  }
+
+  freeaddrinfo(servinfo);
+
+  socklen_t len = 0;
+
+  /* 
+     Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
+  */
+  struct itimerval alarmTime;
+  alarmTime.it_interval.tv_sec=10;
+  alarmTime.it_interval.tv_usec=10;
+  alarmTime.it_value.tv_sec=10;
+  alarmTime.it_value.tv_usec=10;
+
+  /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
+  signal(SIGALRM, checkJobbList);
+  setitimer(ITIMER_REAL,&alarmTime,NULL); // Start/register the alarm. 
+  
+  while(1)
+  {
+    // Check if anything new has arrived
+    int n = recvfrom(sockfd, &servBuf, sizeof(servBuf), MSG_DONTWAIT, &servaddr, &len);
+
+    if(n == sizeof(calcMessage))
+    {
+      // Type-cast calcProtocl to calcMessage
+      calcMessage* message = (calcMessage*)&servBuf;
+      int type = ntohs(message->type);
+      int protocol = ntohs(message->protocol);
+      int major = ntohs(message->major_version);
+      int minor = ntohs(message->minor_version);
+
+      if(type == 22 && protocol == 17 && major == 1 && minor == 0)
+      {
+        // Check if sender is already in system
+        if(!is_in_system(servaddr))
+        {
+          printf("New Client.\n");
+          // Create a new package
+          calcProtocol newPack;
+          create_package(newPack);
+          int send = sendto(sockfd, (const void*)&newPack, sizeof(newPack),
+            0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+          if(send == -1)
+          {
+            perror("Error : Send package");
+          }
+          else
+          {
+            current_pos++;
+          }
+          struct timeval current_time;
+          gettimeofday(&current_time, NULL);
+          timers.push_back(current_time.tv_sec);
+          storage.push_back(newPack);
+        }
+        else
+        {
+          printf("Already in system. Ignoring request.\n");
+        }
+      }
+      else
+      {
+        printf("Wrong Protocol.\n");
+        calcMessage error;
+        error.type = htons(2);
+        error.message = htonl(2);
+        error.protocol = htons(17);
+        error.major_version = htons(1);
+        error.minor_version = htons(0);
+
+        int send = sendto(sockfd, (const void*)&error, sizeof(error),
+            0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        if (send == -1)
+        {
+          perror("Error : Send wrong protocol");
+        }
+      }
+    }
+    else if(n == sizeof(calcProtocol))
+    {
+      // Pack up reponse
+      calcProtocol newProt;
+      newProt.type = ntohs(servBuf.type);
+      newProt.major_version = ntohs(servBuf.major_version);
+      newProt.minor_version = ntohs(servBuf.minor_version);
+      newProt.id = ntohl(servBuf.id);
+      newProt.arith = ntohl(servBuf.arith);
+      newProt.inValue1 = ntohl(servBuf.inValue1);
+      newProt.inValue2 = ntohl(servBuf.inValue2);
+      newProt.inResult = ntohl(servBuf.inResult);
+      newProt.flValue1 = servBuf.flValue1;
+      newProt.flValue2 = servBuf.flValue2;
+      newProt.flResult = servBuf.flResult;
+
+      int spot = 0;
+      int ip = 0;
+      if(is_in_system(newProt.id, spot))
+      {
+        if(spot != 0)
+        {
+          ip = spot;
+        }
+        else
+          ip = 1;
+        
+        if(timers[spot] > 0)
+        {
+          // Is validated, check response, send back result
+          calcMessage resp;
+          resp.message = htonl(2); // Pessimistic start, we assume they respond incorrectly.
+          resp.type = htons(2);
+          resp.protocol = htons(17);
+          resp.major_version = htons(1);
+          resp.minor_version = htons(0);
+        
+          if(newProt.arith >= 5)
+          {
+            if(newProt.flResult == storage[spot].flResult)
+            {
+              printf("Value 1: %8.8g\n", storage[spot].flValue1);
+              printf("Value 2: %8.8g\n", storage[spot].flValue2);
+              printf("Result: %8.8g\n", storage[spot].flResult);
+              resp.message = htonl(1);
+              printf("Float response was correct. %d\n", spot);
+            }
+          }
+          else
+          {
+            if(newProt.inResult == (int)ntohl(storage[spot].inResult))
+            {
+              resp.message = htonl(1);
+              printf("Value 1: %d\n", (int)ntohl(storage[spot].inValue1));
+              printf("Value 2: %d\n", (int)ntohl(storage[spot].inValue2));
+              printf("Result: %d\n", (int)ntohl(storage[spot].inResult));
+              printf("Integer response was correct. %d\n", spot);
+            }
+          }
+          int send = sendto(sockfd, (const void*)&resp, sizeof(resp),
+            0, (struct sockaddr *)&addresses[ip], sizeof(addresses[ip]));
+
+          if(send == -1)
+          {
+            perror("Error : Send confirmation");
+          }
+          else
+          {
+            current_pos--;
+          }
+
+          if(current_pos <= 0)
+          {
+            // This is to empty storage however might lead to unanswered calls from clients.
+            //empty_storage();
+          }
+        }
+        else
+        {
+          // Out of time to respond.
+          printf("Slow client tried to answer, ignoring request.\n");
+          calcMessage error;
+          error.type = htons(2);
+          error.message = htonl(2);
+          error.protocol = htons(17);
+          error.major_version = htons(1);
+          error.minor_version = htons(0);
+
+          sendto(sockfd, (const void*)&error, sizeof(error),
+            0, (const struct sockaddr *)&addresses[ip], sizeof(addresses[ip]));
+        }
+      }
+      else
+      {
+        printf("No match on ID, ignoring request.\n");
+        calcMessage error;
+        error.type = htons(2);
+        error.message = htonl(2);
+        error.protocol = htons(17);
+        error.major_version = htons(1);
+        error.minor_version = htons(0);
+
+        int send = sendto(sockfd, (const void*)&error, sizeof(error),
+          0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+        if(send == -1)
+        {
+          perror("Error : No Matching ID");
+        }
+      }
+    }
+    loopCount++;
+  }
+
+  return(0);
+
+
+  
+}
